e00faf5c6d6c0548f9969e5737527c0fd16613e3
tsuchiya <tsuchiya@bengo4.com>
2021-06-02 00:31:59 +0900

    設定を追加

--- bin/setup.php
+++ bin/setup.php
@@ -1,6 +1,18 @@
 <?php
+use BEAR\Dotenv\Dotenv;
 
-declare(strict_types=1);
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+(new Dotenv())->load(dirname(__DIR__));
 
 chdir(dirname(__DIR__));
-passthru('rm -rf ./var/tmp/*');
+passthru('rm -rf var/tmp/*');
+passthru('chmod 775 var/tmp');
+passthru('chmod 775 var/log');
+// db
+$pdo = new PDO('mysql:host=' . getenv('TKT_DB_HOST'), getenv('TKT_DB_USER'), getenv('TKT_DB_PASS'));
+$pdo->exec('CREATE DATABASE IF NOT EXISTS ' . getenv('TKT_DB_NAME'));
+$pdo->exec('DROP DATABASE IF EXISTS ' . getenv('TKT_DB_NAME') . '_test');
+$pdo->exec('CREATE DATABASE ' . getenv('TKT_DB_NAME') . '_test');
+passthru('./vendor/bin/phinx migrate -c var/phinx/phinx.php -e development');
+passthru('./vendor/bin/phinx migrate -c var/phinx/phinx.php -e test');
